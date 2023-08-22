6e121e3b51debc3c4f08ecfa01e395829176399e
rand0m666br <heitor.vieira.juliano@gmail.com>
2023-08-22 11:48:15 -0300

    front end bina

---
+++ bina2.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Bina</title>
+	<link rel="stylesheet" href="main.css">
+</head>
+
+<body>
+	<form>
+		<div class="img"></div>
+		<div class="teclado">
+			<div class="logo"></div>
+			<div class="nums">
+				<button id="1">1</button>
+				<button id="2">2</button>
+				<button id="3">3</button><br>
+				<button id="4">4</button>
+				<button id="5">5</button>
+				<button id="6">6</button><br>
+				<button id="7">7</button>
+				<button id="8">8</button>
+				<button id="9">9</button><br>
+				<button id="0">0</button>
+				<button id="branco">Branco</button>
+				<button id="Corrige">Corrige</button>
+				<button id="Confirma">Confirma</button>
+			</div>
+		</div>
+	</form>
+</body>
+
+</html>
